35f038546500001f77d538ad21e23c763f485403
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-10 16:08:27 +0530

    Add Indian time for settings file Add media handling url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
+from buying_selling.users import views as user_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
