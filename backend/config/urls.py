8b831c3541369b048a0bdb45c77f8fe53da4fac4
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-07-10 22:21:55 +0530

    fix slash in urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
-    path("api/projects", include("backend.projects.api.urls")),
-    path("api/opensource", include("backend.openSource.api.urls")),
+    path("api/projects/", include("backend.projects.api.urls")),
+    path("api/opensource/", include("backend.openSource.api.urls")),
 ]
 
 if settings.DEBUG:
