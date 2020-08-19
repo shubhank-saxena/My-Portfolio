d3c873e5069753958b4364a71b26be6ed4131162
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-08-19 18:28:46 +0530

    Fix url debug for media files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path("api/devskills/", include("backend.devskills.api.urls")),
 ]
 
-if settings.DEBUG is False:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
