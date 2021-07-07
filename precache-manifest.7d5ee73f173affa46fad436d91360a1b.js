fb41413e37b68c61bc7edf5eca3eab169f52ee5e
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-07-08 01:56:18 +0530

    Updates

---
+++ precache-manifest.7d5ee73f173affa46fad436d91360a1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04df7e12b8584c7178f1f3ef433b0241",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "7f3733a98bb06a11e4e5",
+    "url": "/My-Portfolio/static/js/2.42ca9cda.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/My-Portfolio/static/js/2.42ca9cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b663442452c90dbcabc2",
+    "url": "/My-Portfolio/static/js/main.8f432a5e.chunk.js"
+  },
+  {
+    "revision": "bd26aa8a63ab866d9120",
+    "url": "/My-Portfolio/static/js/runtime-main.295a29ee.js"
+  }
+]);
